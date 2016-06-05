21b3ebf17a2151cd594cf6d0d34aaff12f35c8a3
gschrock <gabrielschrock@gmail.com>
2016-06-05 13:07:31 -0700

    Fixed localStorage issue

--- client/app.js
+++ client/app.js
@@ -111,23 +111,23 @@ angular
 
   .controller('savorCtrl',['$scope', '$http', '$location', '$stateParams', function savorCtrl($scope, $http, $location, $stateParams) {
 
-  function getAll() {
-    var user = JSON.parse(window.localStorage.profile).email;
-    $http.get('/api/restaurants').then(function(res) {
-      $scope.restaurants = _.filter(res.data,function(restaurant) {
-        console.log('user', user);
-        console.log('email', restaurant.userEmail);
-        //filter restaurants such that the email associated with the restaurant is the same as the email of the user currently logged in
-        if(restaurant.userEmail === user) {
-          return true;
-        } else {
-          return false;
-        }
-      });
+  // function getAll() {
+  //   var user = JSON.parse(window.localStorage.profile).email;
+  //   $http.get('/api/restaurants').then(function(res) {
+  //     $scope.restaurants = _.filter(res.data,function(restaurant) {
+  //       console.log('user', user);
+  //       console.log('email', restaurant.userEmail);
+  //       //filter restaurants such that the email associated with the restaurant is the same as the email of the user currently logged in
+  //       if(restaurant.userEmail === user) {
+  //         return true;
+  //       } else {
+  //         return false;
+  //       }
+  //     });
       
-      console.log($scope.restaurants, "console.log $scope.rest");
-    })
-  }
+  //     console.log($scope.restaurants, "console.log $scope.rest");
+  //   })
+  // }
 
   function getOne() {
     var id = $stateParams.id;
@@ -156,7 +156,7 @@ angular
     });
   }
 
-  getAll();
+  // getAll();
 
 }]);
   
--- client/views/components/user/user.ctrl.js
+++ client/views/components/user/user.ctrl.js
@@ -5,7 +5,7 @@ underscore.factory('_', function() {
 angular
   .module('savor.user',['ngMaterial', 'ngMessages', 'material.svgAssetsCache', 'underscore'])
   .controller('userController', function($scope, $http, _) {
-    window.refresh = function() {
+    // window.refresh = function() {
       // $http.get('/api/restaurants').then(function (response) {
       //   console.log('hello');
       //   $scope.restaurants = _.filter(res.data,function(restaurant) {
@@ -19,7 +19,32 @@ angular
       //     }
       //   });
       // })
-    };
+
+    // };
+    $scope.profile = JSON.parse(localStorage.getItem('profile'));
+    
+  function getAll() {
+    var user = JSON.parse(window.localStorage.profile).email;
+    $http.get('/api/restaurants').then(function(res) {
+      $scope.restaurants = _.filter(res.data,function(restaurant) {
+        console.log('user', user);
+        console.log('email', restaurant.userEmail);
+        //filter restaurants such that the email associated with the restaurant is the same as the email of the user currently logged in
+        if(restaurant.userEmail === user) {
+          return true;
+        } else {
+          return false;
+        }
+      });
+      
+      console.log($scope.restaurants, "console.log $scope.rest");
+    });
+  }
+  
+  getAll();
+    
+    
+    
    // window.refresh();
 })
   
