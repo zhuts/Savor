9b78b16971cbbeb463091d9d5f2fce4e5aa9cac5
Andrew Vye <a.vye26@gmail.com>
2016-06-10 16:24:33 -0700

    (fix) Remove login functionality from toolbar

--- client/views/components/toolbar/toolbar.ctrl.js
+++ client/views/components/toolbar/toolbar.ctrl.js
@@ -4,28 +4,11 @@ angular
 
 function toolbarController(auth, store, $location, ngDialog, $scope) {
     var vm = this;
-    vm.login = login;
     vm.logout = logout;
     vm.auth = auth;
     
-    function login() {
-      console.log("Login Success");
-      // The auth service has a signin method that
-      // makes use of Auth0Lock. If authentication
-      // is successful, the user's profile and token
-      // are saved in local storage with the store service
-      auth.signin({}, function(profile, token) {
-        store.set('profile', profile);
-        store.set('token', token);
-        $location.path('/user');
-        if (!$rootScope.$$phase) $rootScope.$apply();
-      }, function(error) {
-        console.log(error);
-      });
-    }
-
     function logout() {
-      console.log("Logout Success")
+      console.log("Logout Success");
       // The signout method on the auth service
       // sets isAuthenticated to false but we
       // also need to remove the profile and
