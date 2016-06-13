fe3e8954dfe4bd708f74a9315e6170bde6579b5f
Andrew Vye <a.vye26@gmail.com>
2016-06-12 18:20:39 -0700

    (feat) Add a POST request to add a friend when that name is clicked

--- client/views/components/friends/friends.js
+++ client/views/components/friends/friends.js
@@ -6,7 +6,7 @@ angular.module('savor.friends',[
   'savor.user'])
 .controller('friendsController', function($scope, $http, ngDialog, Upload, $window, Reviews, Meals) {
   
-  var testList = [];
+  var friendList = [];
   var getAllUsers = function() {
     $http({
       method: "GET",
@@ -17,56 +17,24 @@ angular.module('savor.friends',[
       // an array of all of our user data
       var users = response.data;
       users.forEach(function(currentUser) {
-        testList.push(currentUser.username);
+        friendList.push(currentUser.username);
       });
-      console.log('test here ', testList);
     });
   };
   getAllUsers();
-  // console.log('this is the test ', test);
-  // $scope.userList = getAllUsers();
-$scope.userList = testList;
 
-// ****** DO THIS
-// addFriendToUser = function
-  
-  // *************** Check to ensure all the appropriate fields are here *****************
-  $scope.newReview = function(){
-    
-    var legibleDate = $scope.meal.date.toDateString();
-    var data = ({
+$scope.userList = friendList;
+
+  $scope.addFriendToUser = function(friend) {
+    var data = {
       userID: $scope.userOnRootScope.user_id,
-      meal: $scope.meal.meal,
-      restaurant: $scope.meal.restaurant,
-      notes: $scope.meal.notes,
-      date: legibleDate,
-      image: $scope.myCroppedImageUrl
-    });
-    $scope.addReview($scope.meals, data);
-    $scope.sendPost(data);
-    ngDialog.close();
-  };
-  
-  $scope.sendPost = function (data) {
+      friend: friend
+    };
     $http({
       method: "POST",
-      data: data,
-      url: '/api/users/meals/'
+      url: '/api/users/friends/',
+      data: data
     });
   };
-
-    // gets triggered when a file has been selected from the file input
-    var handleFileSelect = function(evt) {
-      var file = evt.currentTarget.files[0];
-      var reader = new FileReader();
-      reader.onload = function (evt) {
-        console.log('in file reader');
-        $scope.$apply(function($scope){
-          $scope.myImage = evt.target.result;
-        });
-      };
-
-      reader.readAsDataURL(file);
-    };
 
 });
